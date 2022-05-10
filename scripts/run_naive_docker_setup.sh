61520dc0514eec53d29ba33056dd227c52b521cf
Johannes Maier <johannes.maier@active-group.de>
2022-05-10 14:46:38 +0200

    Show service logs in naive setup

--- scripts/run_naive_docker_setup.sh
+++ scripts/run_naive_docker_setup.sh
@@ -6,9 +6,16 @@ docker build -t go-backend-0 ../go_backend
 docker build -t haskell-backend-0 ../haskell_backend
 docker build -t python-client-0 ../python_client
 
-docker run --rm -t -d --network host go-backend-0
-docker run --rm -t -d --network host haskell-backend-0
+docker run --rm --name gob -t -d --network host go-backend-0
+docker run --rm --name hab -t -d --network host haskell-backend-0
 sleep 2
 docker run --rm -it --network host python-client-0
+
+echo "=================================="
+echo "           SERVICE LOGS           "
+echo "=================================="
+
+docker logs gob
+docker logs hab
 
 docker stop $(docker ps -a -q)
