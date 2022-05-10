744aeaf690712ff4bb1f72b0ea04a632ee8735e4
Johannes Maier <johannes.maier@active-group.de>
2022-05-10 14:55:12 +0200

    Add shell script to demo release setup

---
+++ scripts/run_nix_release_setup.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+cd "$(dirname $0)"
+
+nix-build ../nix/docker-release.nix
+docker load < result
+
+docker run --rm --name bob -t -d --network host both-backends
+sleep 2
+docker run --rm -it --network host python-demo
+
+echo "=================================="
+echo "           SERVICE LOGS           "
+echo "=================================="
+
+docker logs bob
+docker stop $(docker ps -a -q)
